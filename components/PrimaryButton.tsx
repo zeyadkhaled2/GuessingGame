9d6d3ff6725322a1b364533a60dfd69957692afd
zeyadkhaled2 <kziad834@gmail.com>
2026-07-04 07:23:49 +0300

    styled a custom button, handling effects within the button range

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 
-function PrimaryButton ({children}) {
+function PrimaryButton({ children }) {
+    function pressHandler() {
+        console.log("Primary button is pressed")
+    }
+
     return (
-        <View>
-            <Text> {children} </Text>
+        <View style={style.buttonOuterContainer}>
+            {/* pressed is a boolean which is true when the button is pressed */}
+            <Pressable style={({pressed}) => pressed ?  [style.buttonInnerContainer, style.pressed] : style.buttonInnerContainer} onPress={pressHandler} android_ripple={{ color: "#640233", foreground: true }}>
+                <Text style={style.buttonText}> {children} </Text>
+            </Pressable>
         </View>
     )
 }
-export default PrimaryButton 
+export default PrimaryButton
+const style = StyleSheet.create({
+    buttonOuterContainer: {
+       borderRadius: 28,
+       margin: 4,
+       overflow: 'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2, // android only shadow
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center"
+    },
+    pressed: {
+        opacity: 0.75,
+
+    }
+
+})
